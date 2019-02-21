1671443e5a945219e50af04c36c165d794c45370
chantist <schantilou@gmail.com>
2019-02-20 21:50:11 -0500

    update configurations for  deployment to heroku

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 const port = process.env.PORT || 8080;
 app.set('port', port);
