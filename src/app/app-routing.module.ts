8e945949efdba5b80c9f84003c7a97d3221c77f8
chantist <schantilou@gmail.com>
2019-02-22 21:28:17 -0500

    added more content and CSS for my home page. Elements added includes routers, home/header/footer components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import{ HomeComponent} from './home/home.component'
+import { ProjectsComponent } from './projects/projects.component';
+
+const routes: Routes = [
+  { path: '**', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [
+    HomeComponent,
+    ProjectsComponent
+  ],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true })
+  ],
+  exports: [RouterModule],
+
+  
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,9 +16,16 @@ import {
   MatToolbarModule
 } from "@angular/material";
 
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -32,7 +39,7 @@ import {
     MatListModule,
     MatToolbarModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: []
 })
 export class AppModule { }
